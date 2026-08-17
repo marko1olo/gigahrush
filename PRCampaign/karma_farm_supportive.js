fca956237ab73c41b0ab56d54effd0d0d29a8107
Петушков А. <96879250+marko1olo@users.noreply.github.com>
2026-08-17 15:49:59 +0000

    chore: remove PRCampaign/ directory
    Marketing campaign artifacts don't belong in the source repository.

--- PRCampaign/karma_farm_supportive.js
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-
-const url = "https://www.reddit.com/r/IndieGaming/new/";
-console.log("Opening r/IndieGaming (New threads)...");
-console.log("INSTRUCTION: Find a new indie dev showing off their game with 0 comments. Tell them it looks great and ask a simple question to support them!");
-
-execSync(`open -a "Google Chrome" "${url}"`);
